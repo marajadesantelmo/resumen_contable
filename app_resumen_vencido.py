ae8dc6d591e31602f14dcc7de79901b566917d92
marajadesantelmo <50368116+marajadesantelmo@users.noreply.github.com>
2025-08-04 08:25:46 -0300

    julio titulo

--- app_resumen_vencido.py
+++ app_resumen_vencido.py
@@ -95,7 +95,7 @@ def to_excel_multiple_sheets(resumen_contable_excel, emitidos_excel, recibidos_e
     return processed_data
 
 def show_page(username):
-    st.title("Resumen Contable - Mes Vencido (Junio 2025)")
+    st.title("Resumen Contable - Mes Vencido (Julio 2025)")
     #st.info("En construcción")
     # Get both formatted data (for display) and raw data (for Excel)
     (
